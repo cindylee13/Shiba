ace643872854fe49deb9b37109305f8fcb12b9ee
Sunny <sunny@SunnydeMacBook-Air.local>
2018-05-22 23:14:09 +0800

    real time

--- mysitenew/nodejs/PriceRealTime.js
+++ mysitenew/nodejs/PriceRealTime.js
@@ -9,16 +9,10 @@ var sub = redis.createClient();
 var sub2 = redis.createClient();
 //Subscribe to the Redis chat channel
 sub.subscribe('price');
-sub2.subscribe('test');
 io.sockets.on('connection', function (socket) {
     //Grab message from Redis and send to client
     sub.on('message', function(channel, message){
     	console.log("message");
-
         socket.send(message);
-    });
-    sub2.on('message', function(channel, message){
-    	console.log("testsets");
-        socket.send("teststst");
     });
 });
