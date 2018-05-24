4108aee0a83cacd49387663dfda17826f1010679
ChiaHsin Lee <r5713410@gmail.com>
2018-05-24 22:30:18 +0800

    我之前改的form 先傳到我自己的branch

--- mysitenew/users/form.py
+++ mysitenew/users/form.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from django import forms  
 class SignUpForm(forms.Form):  
-    username = forms.CharField()  
-    email = forms.EmailField()  
-    password = forms.CharField(widget=forms.PasswordInput)  
-    password2= forms.CharField(label='Confirm',widget=forms.PasswordInput)  
+    username = forms.CharField(max_length=15,label='username', 
+                widget=forms.TextInput(attrs={'placeholder': 'Enter your Username'})) 
+                
+    email= forms.CharField(max_length=100,
+                widget= forms.EmailInput(attrs={'placeholder':'Enter your email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Enter your Password"}))
+    password2= forms.CharField(label='Confirm',widget=forms.PasswordInput(attrs={'placeholder': "Enter your Password"}))
     # 判斷我密碼跟我確認密碼有沒有相同 
     def pwd_validate(self,p1,p2):  
         return p1==p2  
 
 class SignInForm(forms.Form):  
-    username = forms.CharField(label='username', 
+    username = forms.CharField(max_length=15,label='username', 
                 widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Password"}))
 
