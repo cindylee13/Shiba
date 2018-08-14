3f4208a781d82a3396331d63b48c3114736b104d
Frank <frankboygx@gmail.com>
2018-08-14 12:12:52 +0800

    更正MST

--- test/make.py
+++ test/make.py
@@ -10,10 +10,8 @@ typelist = ["Bid","Ask"]
 
 mstlist =[]
 mstnp = []
-coinnp = []
-exchangenp = []
-exchangetype = ""
-cointype = ""
+exchangecointnp = []
+exchangecointtype = ""
 for i in exchangeList:
     for j in  coinList:
         coindict = {"exchange":i,"coin":j}
@@ -27,31 +25,32 @@ def makecoinsort(a,b):
 # print mstlist
 for i in mstlist:
     for j in mstlist:
-        cointype = j["coin"]+i["coin"]
-        exchangetype = j["exchange"]+i["exchange"]
+        exchangecointtype = j["exchange"]+j["coin"]+"->"+i["exchange"]+i["coin"]
         if(i["exchange"]==j["exchange"]):
             num = np.inf
         
         elif(i["coin"]==j["coin"]):
-            num = np.inf
+            num = np.inf    
         else:
             
             if(makecoinsort(j["coin"],i["coin"]) == 0):
                 coin = i["coin"]+j["coin"]
-                num = (1/loaddict[coin][j["exchange"]]["Ask"])-(1/loaddict[coin][i["exchange"]]["Bid"])
+                num = (1/loaddict[coin][i["exchange"]]["Ask"])-(1/loaddict[coin][j["exchange"]]["Bid"])
             elif(makecoinsort(j["coin"],i["coin"]) == 1):
                 coin = j["coin"]+i["coin"]
-                num = loaddict[coin][j["exchange"]]["Ask"]-loaddict[coin][i["exchange"]]["Bid"]
-        coinnp.append(cointype)
+                num = loaddict[coin][i["exchange"]]["Ask"]-loaddict[coin][j["exchange"]]["Bid"]
+        exchangecointnp.append(exchangecointtype)
         mstnp.append(num)
-        exchangenp.append(exchangetype)
-coinarr = np.array(coinnp)
 mst = np.array(mstnp)
-exchangearr = np.array(exchangenp)
-exchangearr.shape = (12,12)
-coinarr.shape = (12,12)
+exchangecointarr = np.array(exchangecointnp)
+exchangecointarr.shape = (12,12)
 mst.shape = (12,12)
-print exchangearr
-print coinarr
+print exchangecointarr
 print mst
-print 0.056308-0.056401
+
+# array:
+#      BTC         ->        ETH
+# X元ETH買1元BTC(ETHBTC_BID)        1元BTC買X元ETH(ETHBTC_ASK)
+
+#      ETH         ->        BTC
+# X元BTC買1元ETH(ETHBTC_BID)        1元ETH買X元BTC(ETHBTC_ASK)
