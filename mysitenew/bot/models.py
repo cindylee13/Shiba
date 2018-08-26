08ffdc5225426c3225cc26a9fbabc352e97c00d8
sunny2920 <lovexu418@gmail.com>
2018-08-27 01:22:20 +0800

    add bot 註解 , add alg table

--- mysitenew/bot/models.py
+++ mysitenew/bot/models.py
@@ -14,9 +14,10 @@ class LineBot(models.Model):
 # Create your models here.
 def FindPerson(event):
      print event.message.text
-     user=User.objects.get(username=event.message.text)
+     user=User.objects.get(username = event.message.text)#這行原本是比對username 現在改為比對亂碼即可
      #user = User.objects.get(userID='2')
-     LineBot.objects.create(LineId=event.source.user_id,UserId=user)
+     LineBot.objects.create(LineId = event.source.user_id,UserId=user)#如果比對符合就創建
+     ########!!!!!!!!要多寫一個比對不符合的防呆!!!!!!!!!#############
      print event.message.text
 def SendMessageByUserId(event,Id):
     lineId = LineBot.objects.filiter(userID=Id)['LineId']
--- mysitenew/trips/admin.py
+++ mysitenew/trips/admin.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import BittrexBTCTable, CexBTCTable, BinanceBTCTable, BitfinexBTCTable, CryptopiaBTCTable
+from .models import AlgTypeByUser,TransectionRecord,BittrexBTCTable, CexBTCTable, BinanceBTCTable, BitfinexBTCTable, CryptopiaBTCTable
+from bot.models import LineBot
 
 admin.site.register(BittrexBTCTable)      #-----1
 admin.site.register(CexBTCTable)          #-----2
 admin.site.register(BinanceBTCTable)      #-----3
 admin.site.register(BitfinexBTCTable)     #-----4
 admin.site.register(CryptopiaBTCTable)    #-----5
+admin.site.register(AlgTypeByUser)     #-----6
+admin.site.register(TransectionRecord)    #-----7
