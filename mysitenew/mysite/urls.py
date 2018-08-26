072941b89e84d153d03e034db053e12476693152
ChiaHsin Lee <r5713410@gmail.com>
2018-08-26 15:41:02 +0800

    - merge urls.py

--- mysitenew/mysite/urls.py
+++ mysitenew/mysite/urls.py
@@ -1,23 +1,26 @@
 from django.conf.urls import include, url  
 from django.contrib import admin  
 from .views import Index
-from users.views import Trading, ForgotPassword, SignUp, SignIn, SignOut, ChangePassword , Error, Order, Withdraw, Deposit
-from users.views import CexWithdraw, BittrexWithdraw, BinanceWithdraw, CexDeposit, BittrexDeposit, BinanceDeposit, CexWallet, BittrexWallet, BinanceWallet
-from trips.views import BTC,Trading,userInfo
+from users.views import Trading, ForgotPassword, SignUp, SignIn, SignOut, ChangePassword , Error , MyProfile, MyWallet, StoredWalletMoney, TakeWalletMoney, Order, Withdraw, Deposit
+from users.views import CexWithdraw, BittrexWithdraw, BinanceWithdraw, CexDeposit, BittrexDeposit, BinanceDeposit, BitfinexDeposit, CexWallet, BittrexWallet, BinanceWallet #, BitfinexWallet, CryptopiaWallet
+from trips.views import BTC,Trading
 #from users.views import ForgotPassword, SignUp, SignIn, SignOut, ChangePassword , Error , MyProfile, MyWallet, StoredWalletMoney, TakeWalletMoney
 admin.autodiscover()  
   
 urlpatterns = [  
-    url(r'^admin/', include(admin.site.urls)),   
+    url(r'^admin/', include(admin.site.urls)),  
     url(r'^BTC/$', BTC),
     url(r'^index/', Index),
     url(r'^users/signin/',SignIn), 
     url(r'^users/forgot/',ForgotPassword), 
     url(r'^users/signup/', SignUp),  
-    url(r'^users/signout/', SignOut),
-    url(r'^userInfo',userInfo),
+    url(r'^users/signout/', SignOut), 
     # url(r'^users/changepassword/(?P<username>\w+)/$',ChangePassword),
     url(r'^users/changepassword/',ChangePassword),
+    url(r'^users/myProfile/',MyProfile),  
+    url(r'^users/myWallet/',MyWallet),  
+    url(r'^users/myWallet/storedWalletMoney/',StoredWalletMoney),  
+    url(r'^users/myWallet/takeWalletMoney/',TakeWalletMoney),  
     url(r'^users/error/', Error), 
     url(r'^users/trading/', Trading), 
     url(r'^users/order/', Order), 
@@ -32,7 +35,7 @@ urlpatterns = [
     url(r'^users/CexWallet/', CexWallet), 
     url(r'^users/BittrexWallet/', BittrexWallet),
     url(r'^users/BinanceWallet/', BinanceWallet),
-    url(r'^bot/', include('bot.urls')),
-    
-
+     url(r'^bot/', include('bot.urls')),
+    #url(r'^users/BitfinexWallet/', BitfinexWallet),
+    #url(r'^users/CryptopiaWallet/', CryptopiaWallet),
 ]
