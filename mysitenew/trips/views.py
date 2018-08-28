b6e94c28128821e611ff4c1fcf6e0492f4d5e42a
sunny2920 <lovexu418@gmail.com>
2018-08-28 21:19:27 +0800

    alg update

--- mysitenew/trips/views.py
+++ mysitenew/trips/views.py
@@ -37,11 +37,8 @@ def BTC(request):
         'CryptopiaBTCTable' : CryptopiaList,
         'dif':dif,
     })
-<<<<<<< HEAD
 
 
-=======
->>>>>>> MST
 def Trading(request):
     #a = BittrexBTC()    #move to crontab
     #print a
